a33da25be5e16ca2b5052f52703d9a4083696383
yaakov hatam <kobieshka@gmail.com>
2021-02-07 21:45:37 +0200

    Add files via upload

--- src/components/data-page.js
+++ src/components/data-page.js
@@ -1,11 +1,36 @@
+import {ADD_GAME_ACTION} from './../redux/actions';
+import { connect } from 'react-redux';
 
-function DataPage() {
-    return (
-      <div>
-          Data page
-      </div>
-    );
+function DataPage(props) {
+
+  const newObj = {
+    HomeTeam: null,
+    AwayTeam: null,
+    Score: null,
+    Date: null,
+    Time: null
   }
+
+  const inputChange = (key, value) => newObj[key] = value;
   
-  export default DataPage;
-  
+  return (
+    <div>
+      {Object.keys(newObj).map(k => <label>{k}: <input onChange={e => inputChange(k, e.target.value)} /></label>)}
+      <button onClick={e => props.ADD_GAME_ACTION(newObj)}>Save</button>
+    </div>
+
+  );
+}
+
+
+function mapStateToProps(state, ownProps) {
+  return {
+  }
+}
+
+const mapDispatchToProps = {
+  ADD_GAME_ACTION
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataPage);
+
